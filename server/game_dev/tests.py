4ce29f9a821134af2a6551f1e1d757d1cec38164
James Lee <jameslee6352@gmail.com>
2025-11-21 19:29:05 +0800

    Adding simple tests

--- server/game_dev/tests.py
+++ server/game_dev/tests.py
@@ -1,3 +1,20 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .models import Member
 
-# Create your tests here.
+
+class MemberModelTest(TestCase):
+    def setUp(self):
+        self.member = Member.objects.create(
+            name="Jane Doe",
+            about="A placeholder who is kindly helping us with testing!",
+            pronouns="She/Her"
+        )
+
+    def test_member_creation(self):
+        try:
+            Member.objects.get(name="Jane Doe")
+        except Member.DoesNotExist:
+            self.fail("Member was not properly created")
+
+    def test_member_is_active(self):
+        self.assertTrue(self.member.active)
