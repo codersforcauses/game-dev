66f1c0d931452f5e0bcbc1d4970191157073cdc8
Karl_Sue <tuankhanh17032003@gmail.com>
2025-11-25 23:30:43 +0000

    feat: add Art model

--- server/game_dev/models.py
+++ server/game_dev/models.py
@@ -10,3 +10,14 @@ class Member(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class Art(models.Model):
+    name = models.CharField(null=False, max_length=200)
+    description = models.CharField(max_length=200,)
+    source_game = models.ForeignKey(Games, on_delete=models.CASCADE, related_name='art_pieces') #Need implement Games model
+    path_to_media = models.CharField(null=False)
+    active = models.BooleanField(null=False)
+
+    def __str__(self):
+        return str(self.name)
