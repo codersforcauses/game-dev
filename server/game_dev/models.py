51ecf1efa4afe57f4cab5c83706794715b5023db
RadinMan <Radinmansouri2018@gmail.com>
2025-11-29 06:36:55 +0000

    Created Games model with fields and sample Event fields for foreignkey

--- server/game_dev/models.py
+++ server/game_dev/models.py
@@ -10,3 +10,39 @@ class Member(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+# Sample Events Class made
+class Events(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    description = models.CharField(max_length=4500)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Games(models.Model):
+
+    # Enum choices
+    class CompletionStatus(models.IntegerField):
+        WIP = 1, "Work in Progress (Unplayable)"
+        PLAYABLE_DEV = 2, "Playable - In Development"
+        BETA = 3, "Beta - Stable but not Final"
+        COMPLETED = 4, "Completed"
+
+    name = models.CharField(max_length=200, null=False)
+    description = models.CharField(max_length=4500)
+    completion = models.IntegerField(
+        choices=CompletionStatus.choices,
+        default=CompletionStatus.WIP,
+        null=False
+    )
+    active = models.BooleanField(default=True, null=False)
+    hostURL = models.CharField(max_length=2083)
+    isItch = models.BooleanField(default=True, null=False)
+    pathToMedia = models.CharField(max_length=2083)
+    event = models.ForeignKey(Events, on_delete=models.CASCADE, null=False)
+    # event reference uuid events.id model
+
+    def __str__(self):
+        return str(self.name)
