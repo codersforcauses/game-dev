660835ba25ac7601ab96839262a4783fc3607d00
saltyypringle <bookreader667@gmail.com>
2025-11-29 15:03:27 +0800

    added some tests for the eventmodel

--- server/game_dev/tests.py
+++ server/game_dev/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from .models import Member
+from .models import Member, Event
+import datetime
 
 
 class MemberModelTest(TestCase):
@@ -18,3 +19,24 @@ class MemberModelTest(TestCase):
 
     def test_member_is_active(self):
         self.assertTrue(self.member.active)
+
+class EventModelTest(TestCase):
+    def setUp(self):
+        self.pub_date = datetime.date(2025, 11, 29)
+        self.event = Event.objects.create(
+            name="Super Fun Event",
+            description="Yayayyayayay!",
+            publicationDate=self.pub_date,
+            location="Ezone",
+        )
+
+    def test_publication_date_is_date(self):
+        event = Event.objects.get(pk=self.event.pk)
+        self.assertIsInstance(event.publicationDate, datetime.date)
+
+    def test_publication_date_matches(self):
+        event = Event.objects.get(pk=self.event.pk)
+        self.assertEqual(event.publicationDate, self.pub_date)
+
+    def test_publication_date_not_empty(self):
+        self.assertTrue(bool(self.event.publicationDate))
