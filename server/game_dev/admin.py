7f91e0b507cce7ff90d4ece816fea4e81faa48b9
Karl_Sue <tuankhanh17032003@gmail.com>
2025-12-06 06:43:13 +0000

    refactor: keep admin register simple

--- server/game_dev/admin.py
+++ server/game_dev/admin.py
@@ -2,23 +2,8 @@ from django.contrib import admin
 from .models import Member, Art, ArtContributor
 
 
-@admin.register(Member)
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ['name', 'active', 'pronouns']
-    list_filter = ['active']
-    search_fields = ['name']
+admin.site.register(Member)
 
+admin.site.register(Art)
 
-@admin.register(Art)
-class ArtAdmin(admin.ModelAdmin):
-    list_display = ['name', 'active']
-    list_filter = ['active']
-    search_fields = ['name']
-
-
-@admin.register(ArtContributor)
-class ArtContributorAdmin(admin.ModelAdmin):
-    list_display = ['art', 'member', 'role']
-    list_filter = ['role']
-    search_fields = ['member__name', 'art__name']
-    autocomplete_fields = ['art', 'member']
+admin.site.register(ArtContributor)
