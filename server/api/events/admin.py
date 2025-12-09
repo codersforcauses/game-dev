485c1f26c1c31e26f10c1209cbced5737b77b96c
Yosuke Inoue <24513446@student.uwa.edu.au>
2025-12-09 05:45:54 +0000

    api/event folder was not necesary

--- server/api/events/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin  # noqa
-
-# Register your models here.
-from .models import Event
-
-
-@admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "publicationDate", "location")
