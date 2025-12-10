342cf6b17c16770c0113f2b56cdebeb9acf6e119
Games4Doritos <evanmiocevich@gmail.com>
2025-12-10 17:40:21 +0800

    formatting

--- client/src/components/ui/featureBox.tsx
+++ client/src/components/ui/featureBox.tsx
@@ -5,24 +5,23 @@ interface contents {
   text: string;
 }
 
-function FeatureBox({ title, text}: contents): React.JSX.Element {
+function FeatureBox({ title, text }: contents): React.JSX.Element {
   return (
     <>
       <div
         style={{
-          boxSizing:'border-box',
+          boxSizing: "border-box",
           clipPath:
             "polygon(0% 0%, 27% 0%, 31% 1rem, 100% 1rem, 100% calc(100% - 1rem), 97% 100%, 70% 100%, 67% calc(100% - 1rem), 0% calc(100% - 1rem))",
         }}
         //Each pair of percentages is a 2d point, and polygon() joins them all together to form a polygon
-        className="m-0 mx-auto bg-secondary pt-[0.15rem] max-w-[60%]"
+        className="m-0 mx-auto max-w-[60%] bg-secondary pt-[0.15rem]"
       >
         <div
           style={{
             width: "calc(100% - 0.3rem)",
-            clipPath:
-              'inherit',
-            boxSizing:'inherit'
+            clipPath: "inherit",
+            boxSizing: "inherit",
           }}
           className="mb-[0.15rem] ml-[0.15rem] bg-[hsl(var(--dark-alt))] p-10"
         >
