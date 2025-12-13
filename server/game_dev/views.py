cd2766b79b6f8ef19efc8a7f1fc28310f5542c3c
RadinMan <Radinmansouri2018@gmail.com>
2025-12-13 03:23:50 +0000

    Added: GamesView that defines the GamesAPI behaviour (CRUD) using serailizer and the queryset

--- server/game_dev/views.py
+++ server/game_dev/views.py
@@ -1,3 +1,11 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .serializers import GamesSerializer
+from .models import Games
+
 
 # Create your views here.
+class GamesView(viewsets.ModelViewSet):
+    serializer_class = GamesSerializer
+    queryset = Games.objects.all()
