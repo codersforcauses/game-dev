b207f44142b89ca27ea81998e12b7d0426d5c7ee
RadinMan <Radinmansouri2018@gmail.com>
2025-12-13 03:02:55 +0000

    Added: GamesAdmin and a sample EventsAdmin that allows Admin to manage Games via /admin/

--- server/game_dev/admin.py
+++ server/game_dev/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member, Games, Events
 
 
 class MemberAdmin(admin.ModelAdmin):
     pass
 
 
+# Sample EventsAdmin Class made
+class EventsAdmin(admin.ModelAdmin):
+    pass
+
+
+class GamesAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "completion", "active", "hostURL", "isItch", "pathToThumbnail", "event")
+
+
 admin.site.register(Member, MemberAdmin)
+admin.site.register(Events, EventsAdmin)
+admin.site.register(Games, GamesAdmin)
