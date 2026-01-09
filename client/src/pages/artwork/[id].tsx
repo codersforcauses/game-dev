e971856e7527274966079badd6694661a02d0f94
David <24074639@student.uwa.edu.au>
2026-01-09 16:33:22 +0000

    style: fix prettier formatting issues in [id].tsx

--- client/src/pages/artwork/[id].tsx
+++ client/src/pages/artwork/[id].tsx
@@ -1,4 +1,4 @@
-import { Instagram,MessageSquare } from "lucide-react";
+import { Instagram, MessageSquare } from "lucide-react";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
