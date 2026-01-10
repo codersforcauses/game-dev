0a8d5883fbe58c1a446ddb1e5d6badbb2d267712
Katariah <kittykatz1344@ucc.asn.au>
2026-01-10 07:39:49 +0000

    u

--- client/src/components/main/MemberProfile.tsx
+++ client/src/components/main/MemberProfile.tsx
@@ -6,5 +6,23 @@
 // import { useState } from "react";
 
 export function MemberProfile() {
-  return <div>Member Component</div>;
+  return (
+    <>
+      <div className="m-auto min-h-80 w-4/5 bg-primary">
+        <h1>hi</h1>
+        <div className="m-auto min-h-60 w-1/5 bg-accent">hello</div>
+        <div className="m-auto w-4/5 bg-muted">
+          wahoo
+          <br />
+          weehee
+        </div>
+        <h2>Projects</h2>
+        <div className="m-auto flex justify-center p-5">
+          <div className="max-w-min border">1</div>
+          <div className="max-w-min border">2</div>
+          <div className="max-w-min border">3</div>
+        </div>
+      </div>
+    </>
+  );
 }
