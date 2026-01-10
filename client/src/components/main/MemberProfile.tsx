4d61e54d825e8af9137a3086a132bcc9ca7b113d
Katariah <kittykatz1344@ucc.asn.au>
2026-01-10 04:41:16 +0000

    Add MemberProfile component and MemberPage

---
+++ client/src/components/main/MemberProfile.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+// import { Menu } from "lucide-react";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { useState } from "react";
+
+export function MemberProfile() {
+  return <div>Member Component</div>;
+}
