45257e2730c549ced807af8b36c1e935edf76560
RadinMan <Radinmansouri2018@gmail.com>
2026-01-14 09:46:28 +0000

    New GameContributorSerializer: passes the contributor information to GameSerializer and gameAPI (double nested)

--- server/game_dev/serializers.py
+++ server/game_dev/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Event
-from .models import Game
+from .models import Game, GameContributors
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -17,7 +17,22 @@ class EventSerializer(serializers.ModelSerializer):
         ]
 
 
+# This is child serializer of GameSerializer
+class GameContributorSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source="member.name")
+
+    class Meta:
+        model = GameContributors
+        fields = ("name", "role")
+
+
 class GamesSerializer(serializers.ModelSerializer):
+    contributors = GameContributorSerializer(
+        many=True,
+        source="game_contributors",
+        read_only=True
+    )
+
     class Meta:
         model = Game
-        fields = ('id', 'name', 'description', 'completion', 'active', 'hostURL', 'isItch', 'pathToThumbnail', 'event')
+        fields = ('id', 'name', 'description', 'completion', 'active', 'hostURL', 'isItch', 'pathToThumbnail', 'event', "contributors")
