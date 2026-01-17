a48d7863d6ce4682f9a1a26fe6e553ed6d76b655
belledw <belledw>
2026-01-17 06:21:07 +0000

    created basic profile outline

--- client/src/components/main/MemberProfile.tsx
+++ client/src/components/main/MemberProfile.tsx
@@ -10,13 +10,17 @@ export function MemberProfile() {
     <>
       <div className="m-auto min-h-80 w-4/5 bg-primary">
         <h1>hi</h1>
-        <div className="m-auto min-h-60 w-1/5 bg-accent">hello</div>
-        <div className="m-auto w-4/5 bg-muted">
-          wahoo
-          <br />
-          weehee
+        <div className="mx-2 flex flex-wrap justify-center gap-y-5 lg:mx-10 lg:justify-start">
+          <div className="mx-2 h-20 w-20 rounded-full bg-accent text-center lg:mx-10">
+            profile picture
+          </div>
+          <div className="w-4/5 bg-muted p-2.5">
+            Name | Pronouns
+            <br />
+            About
+          </div>
         </div>
-        <h2>Projects</h2>
+        <h2 className="mx-10 mt-5">Projects</h2>
         <div className="m-auto flex justify-center p-5">
           <div className="max-w-min border">1</div>
           <div className="max-w-min border">2</div>
