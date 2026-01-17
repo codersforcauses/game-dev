579dee3c7a3eac0103c8f0aa272bbed59e1658b6
belledw <belledw>
2026-01-17 07:26:28 +0000

    added styling for profile outline

--- client/src/components/main/MemberProfile.tsx
+++ client/src/components/main/MemberProfile.tsx
@@ -8,19 +8,19 @@
 export function MemberProfile() {
   return (
     <>
-      <div className="m-auto min-h-80 w-4/5 bg-primary">
-        <h1>hi</h1>
-        <div className="mx-2 flex flex-wrap justify-center gap-y-5 lg:mx-10 lg:justify-start">
-          <div className="mx-2 h-20 w-20 rounded-full bg-accent text-center lg:mx-10">
+      <div className="m-auto h-fit w-4/5 rounded-md bg-primary">
+        <div className="mx-2 flex flex-wrap justify-center gap-y-5 py-10 lg:mx-10">
+          <div className="mr-2 aspect-square overflow-clip rounded-full bg-accent text-center lg:mr-10">
             profile picture
           </div>
-          <div className="w-4/5 bg-muted p-2.5">
-            Name | Pronouns
-            <br />
-            About
+          <div className="h-25 flex w-4/5 flex-col gap-2 rounded-md bg-muted p-2.5 font-firaCode">
+            <div className="text-2xl"> Member Name </div>
+            Pronouns <br /> Bio
           </div>
         </div>
-        <h2 className="mx-10 mt-5">Projects</h2>
+      </div>
+      <div className="m-auto min-h-80 w-4/5">
+        <h2 className="mx-10 mt-5 font-jersey10 text-4xl">Projects</h2>
         <div className="m-auto flex justify-center p-5">
           <div className="max-w-min border">1</div>
           <div className="max-w-min border">2</div>
