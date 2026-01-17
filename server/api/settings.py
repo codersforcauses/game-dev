163cc05634ffa95b79ac4ef116fcd5c4ae03929f
Karl_Sue <24595816@student.uwa.edu.au>
2026-01-17 02:55:09 +0000

    refactor: remove pagination

--- server/api/settings.py
+++ server/api/settings.py
@@ -153,8 +153,3 @@ MEDIA_ROOT = BASE_DIR/"media"
 MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
-}
