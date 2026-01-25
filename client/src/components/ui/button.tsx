8ceb2e26f29961862254bf10ec323983898f7b45
laurenpudz <lhpudney@gmail.com>
2026-01-25 15:12:57 +0800

    remove dark background on outline variant

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-muted-foreground text-muted-foreground bg-background hover:bg-muted-foreground/10",
+          "border border-muted-foreground text-muted-foreground1 hover:bg-muted-foreground/10",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
