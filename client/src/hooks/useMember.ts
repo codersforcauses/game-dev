27b6a3d6508410f2dbc479a575c70dc3b1cd422f
Phillip <phillip.nguyen272@gmail.com>
2026-01-25 02:58:58 +0000

    removed error handling on member hook and only enabled when receiving sanitised correct input

--- client/src/hooks/useMember.ts
+++ client/src/hooks/useMember.ts
@@ -1,30 +1,22 @@
-import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import api from "@/lib/api";
 
 type ApiMember = {
   name: string;
   about: string;
-  pronouns: string;
+  pronouns?: string;
   profile_picture?: string;
-  active: boolean;
 };
 
 // return api member, import id number from router
-export const useMember = (
-  id: number,
-  args?: Omit<UseQueryOptions<ApiMember>, "queryKey" | "queryFn">,
-) => {
+export const useMember = (id?: number) => {
   return useQuery<ApiMember>({
     queryKey: ["member", id],
     queryFn: async () => {
-      const res = await api.get(`/members/${id}/`);
-      if (!res.data.active) {
-        throw new Error("Member is not active; raising error");
-      }
-      return res.data;
+      const response = await api.get(`/members/${id}/`);
+      return response.data;
     },
-    enabled: !!id,
-    ...args,
+    enabled: Number.isFinite(id),
   });
 };
