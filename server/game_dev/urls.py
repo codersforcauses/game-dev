f9b6cc0eb057cb907b495b560dcbde8bd29820d4
belledw <belledw>
2026-02-07 03:34:54 +0000

    Removed duplicate GameShowcase path in urls.py

--- server/game_dev/urls.py
+++ server/game_dev/urls.py
@@ -6,13 +6,11 @@ urlpatterns = [
     path("events/", EventListAPIView.as_view(), name="events-list"),
     path("events/<int:id>/", EventDetailAPIView.as_view()),
     path("games/<int:id>/", GamesDetailAPIView.as_view()),
-    # Updated line for GameShowcase endpoint
-    path("gameshowcase/", GameshowcaseAPIView.as_view(), name="gameshowcase-api"),
-    path('members/<int:id>/', MemberAPIView.as_view()),
-    path("about/", CommitteeAPIView.as_view()),
     path("games/contributor/<int:member>/",
          ContributorGamesListAPIView.as_view()),
     # Updated line for GameShowcase endpoint
     path("gameshowcase/", GameshowcaseAPIView.as_view(), name="gameshowcase-api"),
+    path('members/<int:id>/', MemberAPIView.as_view()),
+    path("about/", CommitteeAPIView.as_view()),
     path('members/<int:id>/', MemberAPIView.as_view())
 ]
