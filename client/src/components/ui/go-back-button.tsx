e130d813bdccb13362aba7d914febd8f4e2ba004
Tuan Khanh Hoang <tuankhanh17032003@gmail.com>
2026-02-07 04:35:50 +0000

    refactor: use svgs instead of inline declaration

--- client/src/components/ui/go-back-button.tsx
+++ client/src/components/ui/go-back-button.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 interface GoBackButtonProps {
@@ -12,22 +13,13 @@ const GoBackButton = ({ url, label }: GoBackButtonProps) => {
         type="button"
       >
         <div className="absolute left-1 top-[4px] z-10 flex h-12 w-1/4 items-center justify-center rounded-xl bg-light_2 duration-500 group-hover:w-[184px]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1024 1024"
-            height="25px"
-            width="25px"
+          <Image
+            src="/go-back-icon.svg"
+            alt=""
+            width={25}
+            height={25}
             aria-hidden="true"
-          >
-            <path
-              d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
-              fill="#000000"
-            />
-            <path
-              d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
-              fill="#000000"
-            />
-          </svg>
+          />
         </div>
         <p className="translate-x-2">{label}</p>
       </button>
