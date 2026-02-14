2676f5d09382d4462c4d3ac95ce055198195d3bc
TobbleCobble <tobble@mythosmc.com>
2026-02-14 04:03:43 +0000

    refactored retroBorder into global.css

--- client/src/components/ui/GameEmbed.tsx
+++ client/src/components/ui/GameEmbed.tsx
@@ -26,7 +26,7 @@ export function GameEmbed({
 
   return (
     <div
-      className={`flex items-center justify-center border-[26px] border-accent bg-background [clip-path:polygon(20px_20px,calc(100%-20px)_20px,100%_32px,100%_30%,calc(100%-20px)_45%,calc(100%-20px)_calc(100%-8px),80%_calc(100%-8px),75%_calc(100%-20px),20px_calc(100%-20px),0%_60%,0%_30%,20px_25%)]`}
+      className={`retroBorder flex items-center justify-center bg-background`}
       style={{ width: gameWidth + 26 * 2, height: gameHeight + 26 * 2 }}
     >
       {!isPlaying ? (
@@ -36,8 +36,8 @@ export function GameEmbed({
             alt="Game Cover"
             width={gameWidth}
             height={gameHeight}
-            className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[calc(-50%-52px)] blur-sm"
-            style={{ width: "auto", height: gameHeight }}
+            className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[calc(-50%-26px)] blur-sm"
+            style={{ width: "auto", height: gameHeight - 26 }}
           />
           <Button
             onClick={() => setIsPlaying(!isPlaying)}
