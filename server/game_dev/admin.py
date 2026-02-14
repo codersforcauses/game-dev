ed9424999a2df878faff802316ddc8c308d4cb3f
RadinMan <Radinmansouri2018@gmail.com>
2026-02-14 07:05:55 +0000

    Added: ArtContributorInline and ArtAdmin to Djnago admin

--- server/game_dev/admin.py
+++ server/game_dev/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Member, Game, Event, GameContributor, GameShowcase, Committee
 
+# from issue-8-merge-40 temp need changes
+from .models import Art, ArtContributor, ArtShowcase
+
 
 class MemberAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "active", "profile_picture", "about", "pronouns")
@@ -29,9 +32,24 @@ class CommitteeAdmin(admin.ModelAdmin):
     raw_id_fields = ["id"]
 
 
+# from issue-8-merge-40 temp need changes
+class ArtContributorInline(admin.TabularInline):
+    model = ArtContributor
+    extra = 1
+
+
+class ArtAdmin(admin.ModelAdmin):
+    inlines = [ArtContributorInline]
+
+
 admin.site.register(Member, MemberAdmin)
 admin.site.register(Event, EventAdmin)
 admin.site.register(Game, GamesAdmin)
 admin.site.register(GameContributor, GameContributorAdmin)
 admin.site.register(GameShowcase, GameShowcaseAdmin)
 admin.site.register(Committee, CommitteeAdmin)
+
+# from issue-8-merge-40 temp need changes
+admin.site.register(Art, ArtAdmin)
+# admin.site.register(ArtContributor)
+admin.site.register(ArtShowcase)
